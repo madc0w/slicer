e0fe839aeda911c9bde5e20f64ed887f7e4b6985
Marc A. Donis <mad7@runbox.com>
2019-07-18 22:12:55 +0200

    polygon triangulation really working this time? used ear clipping https://en.wikipedia.org/wiki/Polygon_triangulation#Ear_clipping_method

--- main.js
+++ main.js
@@ -84,130 +84,217 @@ function slice() {
 		const context = canvas.getContext("2d");
 		context.strokeStyle = "#00ff00";
 		const segments = [];
-		const _points = points["polygon" + polygonId];
+		const _points = [];
+		for (var point of points["polygon" + polygonId]) {
+			_points.push(point); //
+		}
 		console.log("points", _points);
-		for (var i in _points) {
-			if (i > 0) {
-				segments.push([ _points[i - 1], _points[i] ]);
-			} else {
-				segments.push([ _points[i], _points[_points.length - 1] ]);
-			}
-		}
-		for (var i in _points) {
-			i = parseInt(i);
-			for (var j = i + 1; j < _points.length; j++) {
-				j = parseInt(j);
-				const segment = [ _points[i], _points[j] ];
-				for (var k in segments) {
-					k = parseInt(k);
-					var isGood = true;
-					if (isIntersecting(segment, segments[k])) {
-						isGood = false;
-						break;
-					}
-				}
 
-				if (isGood) {
+		while (_points.length > 3) {
+			var ear = findEar(_points);
+			context.beginPath();
+			context.moveTo(ear.segment[0].x, ear.segment[0].y);
+			context.lineTo(ear.segment[1].x, ear.segment[1].y);
+			context.stroke();
 
-					// ensure that segment is not outside of polygon
-					const midpoint = {
-						x : (segment[0].x + segment[1].x) / 2,
-						y : (segment[0].y + segment[1].y) / 2
-					};
-					// reverse x & y to get perpendicular slope
-					const dx = segment[0].y - segment[1].y;
-					const dy = segment[0].x - segment[1].x;
-					var p1,
-						p2;
-					var scaleFactor = 1;
-					do {
-						const scale = scaleFactor++ / (dx * dx + dy * dy);
-						p1 = {
-							x : Math.round(midpoint.x + dx * scale),
-							y : Math.round(midpoint.y + dy * scale)
-						};
-						p2 = {
-							x : Math.round(midpoint.x - dx * scale),
-							y : Math.round(midpoint.y - dy * scale)
-						};
-					} while (distance(p1, p2) < 4);
-
-					const p1Data = context.getImageData(p1.x, p1.y, 1, 1).data;
-					const p2Data = context.getImageData(p1.x, p1.y, 1, 1).data;
-					console.log("i", i);
-					console.log("j", j);
-					console.log("p1", p1);
-					console.log("p1Data", p1Data);
-					console.log("p2", p2);
-					console.log("p2Data", p2Data);
-
-					if (p1Data[0] == 0 && p1Data[1] == 0 && p1Data[2] == 0 &&
-						p2Data[0] == 0 && p2Data[1] == 0 && p2Data[2] == 0) {
-						// outside!
-						console.log("outside");
-						isGood = false;
-					}
-
-					if (isGood) {
-						segments.push(segment);
-						context.beginPath();
-						context.moveTo(segment[0].x, segment[0].y);
-						context.lineTo(segment[1].x, segment[1].y);
-						context.stroke();
-					}
-				}
-			}
+			_points.splice(ear.pointIndex, 1);
 		}
 
-		for (var i in segments) {
-			const segment1 = segments[i];
-			for (var j in segments) {
-				const segment2 = segments[j];
 
-			}
-
-			//			const triangle = {
-			//				points : [ segment[0], segment[1], _points[(i + 1) % _points.length] ]
-			//			};
-
-		}
+	//		for (var i in _points) {
+	//			if (i > 0) {
+	//				segments.push([ _points[i - 1], _points[i] ]);
+	//			} else {
+	//				segments.push([ _points[i], _points[_points.length - 1] ]);
+	//			}
+	//		}
+	//		for (var i in _points) {
+	//			i = parseInt(i);
+	//			for (var j = i + 2; j < _points.length; j++) {
+	//				j = parseInt(j);
+	//				const segment = [ _points[i], _points[j] ];
+	//				for (var k in segments) {
+	//					k = parseInt(k);
+	//					var isGood = true;
+	//					if (isIntersecting(segment, segments[k])) {
+	//						isGood = false;
+	//						break;
+	//					}
+	//				}
+	//
+	//				if (isGood) {
+	//
+	//					// ensure that segment is not outside of polygon
+	//					const midpoint = {
+	//						x : (segment[0].x + segment[1].x) / 2,
+	//						y : (segment[0].y + segment[1].y) / 2
+	//					};
+	//					// reverse x & y to get perpendicular slope
+	//					const dx = segment[0].y - segment[1].y;
+	//					const dy = segment[0].x - segment[1].x;
+	//					var p1,
+	//						p2;
+	//					var scaleFactor = 1;
+	//					do {
+	//						const scale = scaleFactor++ / (dx * dx + dy * dy);
+	//						p1 = {
+	//							x : Math.round(midpoint.x + dx * scale),
+	//							y : Math.round(midpoint.y + dy * scale)
+	//						};
+	//						p2 = {
+	//							x : Math.round(midpoint.x - dx * scale),
+	//							y : Math.round(midpoint.y - dy * scale)
+	//						};
+	//					} while (distance(p1, p2) < 4);
+	//
+	//					const p1Data = context.getImageData(p1.x, p1.y, 1, 1).data;
+	//					const p2Data = context.getImageData(p1.x, p1.y, 1, 1).data;
+	//					console.log("i", i);
+	//					console.log("j", j);
+	//					console.log("p1", p1);
+	//					console.log("p1Data", p1Data);
+	//					console.log("p2", p2);
+	//					console.log("p2Data", p2Data);
+	//
+	//					if (p1Data[0] == 0 && p1Data[1] == 0 && p1Data[2] == 0 &&
+	//						p2Data[0] == 0 && p2Data[1] == 0 && p2Data[2] == 0) {
+	//						// outside!
+	//						console.log("outside");
+	//						isGood = false;
+	//					}
+	//
+	//					if (isGood) {
+	//						segments.push(segment);
+	//						context.beginPath();
+	//						context.moveTo(segment[0].x, segment[0].y);
+	//						context.lineTo(segment[1].x, segment[1].y);
+	//						context.stroke();
+	//					}
+	//				}
+	//			}
+	//		}
+	//
+	//		for (var i in segments) {
+	//			const segment1 = segments[i];
+	//			for (var j in segments) {
+	//				const segment2 = segments[j];
+	//
+	//			}
+	//
+	//			//			const triangle = {
+	//			//				points : [ segment[0], segment[1], _points[(i + 1) % _points.length] ]
+	//			//			};
+	//
+	//		}
 	}
 }
 
 function test() {
 	points.polygon1 = [
+
+		//		{
+		//			x : 86,
+		//			y : 153
+		//		},
+		//		{
+		//			x : 104,
+		//			y : 304
+		//		},
+		//		{
+		//			x : 177,
+		//			y : 156
+		//		},
+		//		{
+		//			x : 143,
+		//			y : 42
+		//		},
+		//		{
+		//			x : 156,
+		//			y : 180
+		//		}
+
 		{
 			x : 86,
+			y : 153
+		},
+		{
+			x : 104,
+			y : 304
+		},
+		{
+			x : 243,
+			y : 153
+		},
+		{
+			x : 231,
+			y : 299
+		},
+		{
+			x : 380,
+			y : 229
+		},
+		{
+			x : 404,
 			y : 133
 		},
 		{
-			x : 72,
-			y : 289
+			x : 318,
+			y : 32
 		},
 		{
-			x : 132,
-			y : 200
+			x : 344,
+			y : 172
 		},
 		{
-			x : 160,
-			y : 326
+			x : 220,
+			y : 41
 		},
 		{
-			x : 204,
-			y : 187
+			x : 177,
+			y : 156
 		},
 		{
-			x : 246,
-			y : 337
+			x : 143,
+			y : 42
 		},
 		{
-			x : 266,
-			y : 167
+			x : 156,
+			y : 180
 		},
-		{
-			x : 160,
-			y : 38
-		} ];
+
+	//		{
+	//			x : 86,
+	//			y : 133
+	//		},
+	//		//		{
+	//		//			x : 72,
+	//		//			y : 289
+	//		//		},
+	//		//		{
+	//		//			x : 132,
+	//		//			y : 200
+	//		//		},
+	//		{
+	//			x : 160,
+	//			y : 326
+	//		},
+	//		{
+	//			x : 204,
+	//			y : 187
+	//		},
+	//		//		{
+	//		//			x : 246,
+	//		//			y : 337
+	//		//		},
+	//		//		{
+	//		//			x : 266,
+	//		//			y : 167
+	//		//		},
+	//		{
+	//			x : 160,
+	//			y : 38
+	//		} 
+	];
 	points.polygon2 = [
 		{
 			x : 155,
@@ -342,4 +429,63 @@ function isPointIn(point, _points) {
 		} //
 	}
 	return false;
+}
+
+function findEar(_points) {
+	const canvas = $("#test-canvas").get(0);
+	const context = canvas.getContext("2d");
+	context.clearRect(0, 0, canvas.height, canvas.width);
+	// for obscure reasons, clearRect refuses to clear the whole canvas.  fillRect has the same problem.  so......
+	context.clearRect(canvas.width / 2, 0, canvas.height, canvas.width);
+	context.fillStyle = "rgba(255, 20, 0, 0.4)";
+	context.font = "30px Arial";
+	const prevPoint = _points[_points.length - 1];
+	context.beginPath();
+	context.moveTo(prevPoint.x, prevPoint.y);
+	for (var point of _points) {
+		context.lineTo(point.x, point.y); //
+	}
+	context.closePath();
+	context.fill();
+
+	//	for (var i in _points) {
+	//		const point = _points[i];
+	//		context.fillText(i, point.x, point.y); //
+	//	}
+
+	var segment = null;
+	for (var i in _points) {
+		i = parseInt(i);
+		segment = [ _points[i], _points[(i + 2) % _points.length] ];
+
+		var isOutside = false;
+		// ensure that segment is not outside of polygon
+		const dx = segment[1].x - segment[0].x;
+		const dy = segment[1].y - segment[0].y;
+		const numSteps = 32;
+		for (var j = 1; j < numSteps; j++) {
+			const midpoint = {
+				x : segment[0].x + (j * dx / numSteps),
+				y : segment[0].y + (j * dy / numSteps)
+			};
+
+			const pData = context.getImageData(midpoint.x, midpoint.y, 1, 1).data;
+			console.log("i", i);
+			console.log("step", j);
+			console.log("pData", pData);
+
+			if (pData[0] == 0 && pData[1] == 0 && pData[2] == 0) {
+				// outside
+				isOutside = true;
+				break;
+			}
+		}
+		if (!isOutside) {
+			return {
+				segment : segment,
+				pointIndex : i + 1
+			};
+		}
+	}
+	return null;
 }
